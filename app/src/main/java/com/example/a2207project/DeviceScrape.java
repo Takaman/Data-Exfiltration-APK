bfe2dd3e04d966a163615afa1961f506de6b5f13
Mosipa <jiaric@hotmail.com>
2023-02-03 16:31:45 +0800

    Update DeviceScrape.java

--- app/src/main/java/com/example/a2207project/DeviceScrape.java
+++ app/src/main/java/com/example/a2207project/DeviceScrape.java
@@ -10,16 +10,10 @@ public class DeviceScrape {
         StringBuilder deviceInfo = new StringBuilder();
         deviceInfo.append("\n OS version: "+ System.getProperty("os.version")+ "(" + Build.VERSION.INCREMENTAL + ")");
         deviceInfo.append("\n OS API level:" + Build.VERSION.SDK_INT);
-        deviceInfo.append("\n BootLoader: "+ Build.BootLoader);
-        deviceInfo.append("\n User: "+ Build.User);
         deviceInfo.append("\n Device: "+ Build.DEVICE);
         deviceInfo.append("\n ID: "+ Build.ID);
-        deviceInfo.append("\n Serial Number: "+ Build.Serial);
         deviceInfo.append("\n Brand+Manufacturer: " + Build.BRAND +" " +Build.MANUFACTURER);
         deviceInfo.append("\n Model (and Product): "+ Build.MODEL + " ("+ Build.PRODUCT+")");
-        deviceInfo.append("\n Hardware: "+ Build.Hardware);
-        deviceInfo.append("\n CPU: "+ Build.CPU_ABI);
-        deviceInfo.append("\n Display: "+ Build.Display);
 
         return deviceInfo.toString();
     }
